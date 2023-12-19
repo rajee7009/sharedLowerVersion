3aae41c152dbdae04b46c7d6150023a9de67d359
Durairaj_Rajendiran <>
2023-12-19 10:35:44 +0530

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class AppModule { }
